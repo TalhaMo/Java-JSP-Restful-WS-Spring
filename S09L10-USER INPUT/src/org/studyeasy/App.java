602ef0e1fab53b8d67e64d8a89e3512f0c0df379
TalhaMo <talhamohamed23@yahoo.com>
2022-07-16 20:44:30 +0100

    input

---
+++ S09L10-USER INPUT/src/org/studyeasy/App.java
@@ -0,0 +1,17 @@
+package org.studyeasy;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println("whats's your name");
+		Scanner sc= new Scanner(System.in);
+		String name=sc.next();
+		System.out.println("hey there "+name+" have a nice day");
+		sc.close();
+		
+	}
+
+}
